add2c620cdd5c74b3515a2c01b2fad3f2fbab665
imanuelcdw <icrucifixio@gmail.com>
2022-08-24 19:05:56 +0700

    fix cors

--- app.js
+++ app.js
@@ -23,7 +23,14 @@ const projectRouter = require('./routes/project')
 
 // Main
 app.use('/api/project', projectRouter)
-app.use('/', (req, res) => res.json('/api/project'))
+app.use('/', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.setHeader('Access-Control-Max-Age', '1800')
+  res.setHeader('Access-Control-Allow-Headers', 'content-type')
+  res.setHeader('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, PATCH, OPTIONS')
+  res.json('/api/project')
+})
 app.use(errorHandler)
 
 // APP
